da2caae16c424e0ff7ebf4a3ff68b2293223edc9
Maksim <95shum@gmail.com>
2023-09-05 14:05:41 +0300

    add menu on day

--- js/script.js
+++ js/script.js
@@ -136,21 +136,23 @@ window.addEventListener("scroll", scroll);
 // Request
 
 const inName = document.querySelector(".inName"),
-inPhone = document.querySelector(".inPhone");
+  inPhone = document.querySelector(".inPhone");
 let reqButton = document.querySelector(".req");
 if (inName.value || inPhone.value) {
   reqButton.setAttribute(disabled, true);
 }
-console.log(modal)
-modal.addEventListener('click', (e) => {if(e.target.classList.contains('modal__close')){
-  console.log(e.target);modalClose()}})
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal__close")) {
+    modalClose();
+  }
+});
 
 reqButton.addEventListener("click", (e) => {
   if (!inName.value || !inPhone.value) {
     reqButton.setAttribute(disabled, true);
   } else {
     e.preventDefault();
-    fetch("https://jsonplaceholder.typicode.com/posts", {
+    fetch("http://localhost:3000/requests", {
       method: "POST",
       body: JSON.stringify({
         name: inName.value,
@@ -176,10 +178,9 @@ reqButton.addEventListener("click", (e) => {
                     <div class="modal__title">Cпасибо,мы с вами свяжемся!</div>
                 </div>
             `;
-      
-              
+
       document.querySelector(".modal").append(thanksModal);
-      
+
       setTimeout(() => {
         thanksModal.remove();
         modalClose();
@@ -191,3 +192,32 @@ reqButton.addEventListener("click", (e) => {
     showThanksModal();
   }
 });
+
+// Menu on day
+
+const getCard = async (url) => {
+  const res = await fetch(url);
+
+  return await res.json();
+};
+getCard("http://localhost:3000/menu").then((data) => {
+  data.forEach((obj) => {
+    createCard(obj.img,obj.altimg,obj.title,obj.descr,obj.price)
+  });
+});
+
+function createCard(img, altimg, title, descr, price) {
+  const element = document.createElement("div");
+  element.classList.add("menu__item");
+  element.innerHTML = `
+    <img src=${img} alt=${altimg}>
+  <h3 class="menu__item-subtitle">${title}</h3>
+  <div class="menu__item-descr">${descr}</div>
+  <div class="menu__item-divider"></div>
+  <div class="menu__item-price">
+  <div class="menu__item-cost">цена:</div>
+  <div class="menu__item-total"><span>${price}</span> рублей/день</div>
+  </div>
+    `;
+  document.querySelector(".menu .container").append(element);
+}
