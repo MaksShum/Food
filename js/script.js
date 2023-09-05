26150ef50dc64d31263319c74710b8ea20b5d261
Maksim <95shum@gmail.com>
2023-09-05 20:25:52 +0300

    add-calculating

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ function scroll() {
     window.removeEventListener("scroll", scroll);
   }
 }
-const timerModalOpen = setTimeout(modalOpen, 3000);
+const timerModalOpen = setTimeout(modalOpen, 15000);
 window.addEventListener("scroll", scroll);
 
 // Request
@@ -224,35 +224,119 @@ function createCard(img, altimg, title, descr, price) {
 
 // Slider
 
-    const current = document.querySelector("#current"),
-    prev = document.querySelector(".offer__slider-prev"),
-    next = document.querySelector(".offer__slider-next")
-    let count = 1;
-    const pictures = document.querySelectorAll('.offer__slide')
-    pictures.forEach(item => item.style.display = 'none')
-    pictures[count - 1].style.display = 'block'
+const current = document.querySelector("#current"),
+  prev = document.querySelector(".offer__slider-prev"),
+  next = document.querySelector(".offer__slider-next");
+let count = 1;
+const pictures = document.querySelectorAll(".offer__slide");
+pictures.forEach((item) => (item.style.display = "none"));
+pictures[count - 1].style.display = "block";
 
-
-  function prevCount() {
-    pictures.forEach(item => item.style.display = 'none')
-    if(count < 2){
-      count = 4
-      }
-      else{
-      count--;} 
-      current.textContent = `0${count}`
-      pictures[count - 1].style.display = 'block' 
+function prevCount() {
+  pictures.forEach((item) => (item.style.display = "none"));
+  if (count < 2) {
+    count = 4;
+  } else {
+    count--;
   }
-  function nextCount() {
-    pictures.forEach(item => item.style.display = 'none')
-    if(count > 3 ){
-      count = 1
+  current.textContent = `0${count}`;
+  pictures[count - 1].style.display = "block";
+}
+function nextCount() {
+  pictures.forEach((item) => (item.style.display = "none"));
+  if (count > 3) {
+    count = 1;
+  } else {
+    count++;
+  }
+  current.textContent = `0${count}`;
+  pictures[count - 1].style.display = "block";
+}
+prev.addEventListener("click", prevCount);
+next.addEventListener("click", nextCount);
+
+// Calculating
+
+const size = document.querySelectorAll(
+  ".calculating__choose calculating__choose_medium"
+);
+const heightElem = document.querySelector("#height");
+const weightElem = document.querySelector("#weight");
+const ageElem = document.querySelector("#age");
+const buttonPerson = document.querySelectorAll(".person");
+const norma = document.querySelector(".calculating__result");
+const res = norma.querySelector("span");
+let sex = "female",
+  active = 1.55,
+  weight,
+  age,
+  height;
+
+function getFormula() {
+  if (!weight || !age || !height) {
+    return "____";
+  } else {
+    if (sex == "male") {
+      res.textContent = (
+        88.36 +
+        13.4 * weight +
+        4.8 * height -
+        5.7 * age * active
+      ).toFixed(2);
+    } else if (sex == "female") {
+      res.textContent = (
+        447.6 +
+        9.2 * weight +
+        3.1 * height -
+        4.3 * age * active
+      ).toFixed(2);
     }
-      else{
-        count++;
-      } 
-      current.textContent = `0${count}`
-        pictures[count - 1].style.display = 'block' 
   }
-  prev.addEventListener("click", prevCount);
-  next.addEventListener("click", nextCount);
+}
+
+function chosePerson(e) {
+  buttonPerson.forEach((item) => {
+    item.classList.remove("calculating__choose-item_active");
+    e.target.classList.add("calculating__choose-item_active");
+    if (e.target.innerHTML == "Мужчина") {
+      sex = "male";
+    } else {
+      sex = "female";
+    }
+  });
+  getFormula();
+}
+
+const buttonActive = document.querySelectorAll(".active");
+function choseActive(e) {
+  buttonActive.forEach((item) => {
+    item.classList.remove("calculating__choose-item_active");
+    e.target.classList.add("calculating__choose-item_active");
+    e.target.getAttribute("data-ratio");
+    active = +e.target.getAttribute("data-ratio");
+    getFormula();
+  });
+}
+
+buttonActive.forEach((item) => {
+  item.addEventListener("click", choseActive, getFormula);
+});
+buttonPerson.forEach((item) => {
+  item.addEventListener("click", chosePerson, getFormula());
+});
+
+heightElem.addEventListener("input", () => {
+  height = heightElem.value;
+
+  getFormula();
+});
+weightElem.addEventListener("input", () => {
+  weight = weightElem.value;
+
+  getFormula();
+});
+ageElem.addEventListener("input", () => {
+  age = ageElem.value;
+
+  getFormula();
+});
